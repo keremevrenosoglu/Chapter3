e091cac4d9a05298655db15b7f0a8ac0d0003103
keremevrenosoglu <31323087+keremevrenosoglu@users.noreply.github.com>
2017-10-19 12:37:38 -0700

    Chapter3

---
+++ ChapterThree/ChargeAccount.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class ChargeAccount here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner; 
+public class ChargeAccount
+{
+    public static void main (String[] args) {
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.println ("CS CARD International Statement");
+      System.out.println ("===============================");
+      System.out.println ("Previous Balance:      $");
+      System.out.println ("Additional Charges:    $");
+      System.out.println ("Interest:              $");
+      System.out.println ("New Balance:           $");
+      System.out.println ("Minimum Payment:       $");
+
+      
+    
+    
+}
+}
---
+++ ChapterThree/Rock.java
@@ -0,0 +1,28 @@
+package ChapterThree;
+
+
+// ****************************************************************
+//   Rock.java
+//
+//   Play Rock, Paper, Scissors with the user
+//          
+// ****************************************************************
+import java.util.Scanner;
+import java.util.Random;
+public class Rock
+{
+    public static void main(String[] args)
+    {
+        String personPlay;    //User's play -- "R", "P", or "S"
+        String computerPlay;  //Computer's play -- "R", "P", or "S"
+        int computerInt;      //Randomly generated number used to determine
+                              //computer's play
+        Random generator = new Random();
+        System.out.println ("Type R for rock, P for paper, or S for scissors") //Get player's play -- note that this is stored as a string
+        //Make player's play uppercase for ease of comparison
+        //Generate computer's play (0,1,2)
+        //Translate computer's randomly generated play to string
+        //Print computer's play"
+        //See who won.
+    }
+}
